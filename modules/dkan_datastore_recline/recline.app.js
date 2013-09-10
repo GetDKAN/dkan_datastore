d50bf32b29bf25e405e12a9df847eb33c5d17920
acouch <acinternets@gmail.com>
2013-09-10 16:29:51 -0500

    Updating datastore api to be ckan compatible and updating feeds importer to use link field properly

--- modules/dkan_datastore_recline/recline.app.js
+++ modules/dkan_datastore_recline/recline.app.js
@@ -17,7 +17,7 @@
 // 2) if link to file use dataproxy
 // 3) field formatter checks if api exists, uses dataproxy if available, else
 // uses csv, keeps setting in field formatter settings
-// 4) 
+// 4)
 //
 //
 //
@@ -37,10 +37,13 @@
       graph = Drupal.settings.recline.graph;
       map = Drupal.settings.recline.map;
       uuid = Drupal.settings.recline.uuid;
+      dkan = Drupal.settings.recline.dkan;
+      fileType = Drupal.settings.recline.fileType;
 
       window.dataExplorer = null;
       window.explorerDiv = $('.data-explorer');
 
+      // This is the very basic state collection.
       var state = recline.View.parseQueryString(decodeURIComponent(window.location.hash));
       if ('#map' in state) {
         state['currentView'] = 'map';
@@ -49,36 +52,76 @@
       } else if ('#timeline' in state) {
         state['currentView'] = 'timeline';
       }
-      var DKAN_API = '/api/action/datastore/search.json';
-      var url = window.location.origin + DKAN_API + '?resource_id=' + uuid;
-      var DkanDatastore = false;
-      var DkanApi = $.ajax({
-        type: 'GET',
-        url: url,
-        dataType: 'json',
-        success: function(data, status) {
-          if ('success' in data && data.success) {
-            DkanDatastore = true;
+      if (dkan) {
+        var DKAN_API = '/api/action/datastore/search.json';
+        var url = window.location.origin + DKAN_API + '?resource_id=' + uuid;
+        var DkanDatastore = false;
+        var DkanApi = $.ajax({
+          type: 'GET',
+          url: url,
+          dataType: 'json',
+          success: function(data, status) {
+            if ('success' in data && data.success) {
+              DkanDatastore = true;
+            }
+          },
+          error: function(data, status) {
+            $('.data-explorer').append('<div class="messages status">Unable to connect to the datastore.</div>');
+          },
+        });
+        DkanApi.done(function(data) {
+          if (DkanDatastore) {
+            var dataset = new recline.Model.Dataset({
+              endpoint: window.location.origin + '/api',
+              url: url,
+              id: uuid,
+              backend: 'ckan',
+            });
+            dataset.fetch();
+            return createExplorer(dataset, state);
           }
-        },
-      });
-      DkanApi.done(function(data) {
-        if (DkanDatastore) {
-          var dataset = new recline.Model.Dataset({
-            uuid: uuid,
-            url: url,
-            backend: 'dkan',
-          });
-        } else if (fileType = 'text/csv') {
-          var dataset = new recline.Model.Dataset({
-             url: file,
-             backend: 'csv',
-          });
-        // TODO: check filetype if xls.
-        }
+          else {
+          }
+        });
+      }
+      if (fileType == 'text/csv') {
+        $.ajax({
+          url: file,
+          timeout: 1000,
+          success: function(data) {
+            var dataset = new recline.Model.Dataset({
+               data: data,
+               backend: 'csv',
+            });
+            dataset.fetch();
+            createExplorer(dataset, state);
+          },
+          error: function(x, t, m) {
+            if (t === "timeout") {
+              $('.data-explorer').append('<div class="messages status">File was too large or unavailable for preview.</div>');
+            } else {
+              $('.data-explorer').append('<div class="messages status">Data preview unavailable.</div>');
+            }
+          }
+        });
+      // TODO: check filetype if xls.
+      }
+      else if (fileType == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' || fileType == 'application/vnd.ms-excel') {
+        var dataset = new recline.Model.Dataset({
+          url: file,
+          backend: 'dataproxy',
+        });
         dataset.fetch();
-        createExplorer(dataset, state);
-      });
+        if (dataset.recordCount) {
+          createExplorer(dataset, state);
+        }
+        else {
+          $('.data-explorer').append('<div class="messages status">Unable to retreive data for selected file. This may be do to an interuption with the DataProxy service.</div>');
+        }
+      }
+      else {
+        $('.data-explorer').append('<div class="messages status">File type ' + fileType + ' not supported for preview.</div>');
+      }
     }
   }
 
